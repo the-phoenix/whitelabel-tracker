4cad9e97a2263d7001eedb5cab00a9acc75daf35
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2020-12-22 09:15:12 -0500

    add redux store

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,25 +1,13 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { sum } from "../core/math";
+import React from "react";
+import { Provider } from 'react-redux';
+import { store } from './stores';
+import Home from './containers/Home';
 
 function App() {
-  const [serverResult, setServerResult] = useState<number | null>(null);
-  useEffect(() => {
-    (async () => {
-      const result = await fetch("/api/sum");
-      const newServerResult = await result.json();
-      setServerResult(newServerResult);
-    })();
-  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Client result {sum(1, 3)}</p>
-        <p>Server result {serverResult}</p>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Home />
+    </Provider>
   );
 }
 
---
+++ src/client/containers/Home/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import logo from "./logo.svg";
+import "./Home.css";
+import { selectCount } from '../../stores/counter'
+import { sum } from "../../../core/math";
+
+export default function Home() {
+  const count = useSelector(selectCount);
+  const [serverResult, setServerResult] = useState<number | null>(null);
+  useEffect(() => {
+    (async () => {
+      const result = await fetch("/api/sum");
+      const newServerResult = await result.json();
+      setServerResult(newServerResult);
+    })();
+  }, []);
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>Client result {sum(1, 3)}</p>
+        <p>Server result {serverResult}</p>
+        <p>Count {count}</p>
+      </header>
+    </div>
+  );
+}
